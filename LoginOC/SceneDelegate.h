c0f0a77409ef8121cfeab07f9a3b1f65c5617567
yuhongpeng <yuhongpeng@lixiang.com>
2020-04-17 23:38:24 +0800

    feat: 修改项目可运行

---
+++ LoginOC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LoginOC
+//
+//  Created by 于鸿鹏 on 2020/4/17.
+//  Copyright © 2020 于鸿鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
