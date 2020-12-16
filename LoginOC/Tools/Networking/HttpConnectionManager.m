9670abd70b377ecc90a8885c7524534bd1d330e9
于鸿鹏 <hongpeng03@gmail.com>
2020-12-17 00:43:06 +0800

    fix: 处理304.增加NSURL缓存

--- LoginOC/Tools/Networking/HttpConnectionManager.m
+++ LoginOC/Tools/Networking/HttpConnectionManager.m
@@ -60,13 +60,10 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
  串行队列，保证回调的正确顺序，处理请求返回的结果，如果为nil，会创建一个
  */
 @property (nonatomic, strong) NSOperationQueue *operationQueue;
-
 @property (nonatomic, strong) NSMutableDictionary *taskContext;
-
+@property (nonatomic, strong) NSMutableDictionary *cachedDatas;
 @property (nonatomic, strong) NSMutableArray *mutableTasks;
-
 @property (readwrite, nonatomic, strong) NSLock *lock;
-
 @property (nonatomic, assign) NSUInteger runningTaskCount;
 
 @end
@@ -90,6 +87,7 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
 {
     self = [super init];
     if (self) {
+        self.cachedDatas = [NSMutableDictionary dictionary];
         self.taskDatas = [NSMutableDictionary dictionary];
         self.taskContext = [NSMutableDictionary dictionary];
         self.mutableTasks = [NSMutableArray array];
@@ -99,6 +97,9 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
         // 使用 NSLock 性能较好，防止多线程问题
         self.lock = [[NSLock alloc] init];
         self.lock.name = HttpConnectionSessionManagerLockName;
+        
+        NSURLCache *cache = [self defaultURLCache];
+        [NSURLCache setSharedURLCache:cache];
         
         /**
          设置Session配置，如缓存策略、超时、代理、管道、要支持的TLS版本、cookie策略、凭据存储等。默认缓存是使用磁盘缓存
@@ -135,6 +136,25 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
     // 设置cache
     mutableRequest.cachePolicy = NSURLCacheStorageAllowed;
     
+    // 设置缓存
+    BOOL isCacheData = NO;
+    
+    [self.lock lock];
+    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:mutableRequest];
+    [self.lock unlock];
+    
+    if (cachedResponse && cachedResponse.data) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)cachedResponse.response;
+        // 响应HTTP报头，服务端给客户端
+        NSString *lastModifiedValue = httpResponse.allHeaderFields[@"Last-Modified"];
+        if (lastModifiedValue) {
+            // 客户端给服务端的http报文头部，最后修改时间是否过期，未过期 304
+            [mutableRequest setValue:lastModifiedValue forHTTPHeaderField:@"If-Modified-Since"];
+        }
+        isCacheData = YES;
+    }
+    
+    
     NSURLSessionTask *task = [self.defaultSession dataTaskWithRequest:mutableRequest];
     NSString *taskKey = [self getTaskKeyForTask:task];
     
@@ -149,7 +169,12 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
     [self.lock lock];
     self.taskContext[taskKey] = taskContext;
     [self.mutableTasks addObject:task];
+    
+    if (isCacheData) {
+        self.cachedDatas[taskKey] = cachedResponse.data;
+    }
     [self.lock unlock];
+    
     
     [self excuteReuqestIfNeeded];
     
@@ -159,7 +184,6 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
 - (NSString *)getTaskKeyForTask: (NSURLSessionTask*)task {
     return [NSString stringWithFormat:@"%lu", task.taskIdentifier];
 }
-
 
 - (void)excuteReuqestIfNeeded {
     if (self.runningTaskCount >= MAX_TASK_COUNT || self.mutableTasks.count == 0) {
@@ -173,6 +197,14 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
 
     [willRunTask resume];
     self.runningTaskCount += 1;
+}
+
+#pragma mark - Cache
+
+- (NSURLCache *)defaultURLCache {
+    return [[NSURLCache alloc] initWithMemoryCapacity:20 * 1024 * 1024
+                                         diskCapacity:150 * 1024 * 1024
+                                             diskPath:@"HttpConnectionDisKCache"];
 }
 
 #pragma mark -
@@ -202,9 +234,24 @@ static NSString * const HttpConnectionSessionManagerLockName = @"HTTP.networking
         response = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
     }
     
+    
+    // 304
+    NSData *cacheData = self.cachedDatas[taskKey];
+    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+    if (httpResponse.statusCode == 304 && self.cachedDatas) {
+        response = [NSJSONSerialization JSONObjectWithData:cacheData options:0 error:nil];;
+    }
+    
+    NSString *lastModifiedValue = httpResponse.allHeaderFields[@"Last-Modified"];
+    if (lastModifiedValue && responseData) {
+        NSCachedURLResponse *cachedResponse = [[NSCachedURLResponse alloc] initWithResponse:task.response data:responseData];
+        [[NSURLCache sharedURLCache] storeCachedResponse:cachedResponse forRequest:task.originalRequest];
+    }
+    
     [self.lock lock];
     [self.taskDatas removeObjectForKey:taskKey];
     [self.taskContext removeObjectForKey:taskKey];
+    [self.cachedDatas removeObjectForKey:taskKey];
     [self.lock unlock];
     
     self.runningTaskCount -= 1;
